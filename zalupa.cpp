6942a3f7452acf21ad19405522f66c612cff489e
Tameratlant <80047396+Tameratlant@users.noreply.github.com>
2022-05-02 11:34:53 +0300

    Fixed style

--- zalupa.cpp
+++ zalupa.cpp
@@ -1,30 +1,28 @@
 #include <SFML/Graphics.hpp>
 
+#define RAD 50
+#define FPS 30
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(640, 480), "WINDOW_TITLE");
-    window.setFramerateLimit(50);
-    sf::CircleShape circle(50.f);
+sf::RenderWindow window(sf::VideoMode(800, 600), "WINDOW_TITLE");
+
+void zalupa(sf::CircleShape circle) {                           //На вход принимает круг, для которого уже прописаны все параметры. Рисует его там, где щелкнула мышь
+    
     while (window.isOpen())
     {
         sf::Event event;
-        
         while (window.pollEvent(event))
         {
             switch (event.type)
             {
-                case sf::Event::Closed:
-                {
-                    window.close();
-                    return 0;
-                }
-                case sf::Event::MouseButtonPressed:
-                {
-                    circle.setOutlineThickness(15.f); // устанавливаем толщину контура круга
-                    circle.setOutlineColor(sf::Color(80, 220, 50)); // устанавливаем цвет контура
-                    circle.setPosition(event.mouseButton.x, event.mouseButton.y);
-                }
+            case sf::Event::Closed:
+            {
+                window.close();
+                return;
+            }
+            case sf::Event::MouseButtonPressed:
+            {  
+                circle.setPosition(event.mouseButton.x - RAD, event.mouseButton.y - RAD);
+            }
             }
         }
 
@@ -32,4 +30,18 @@ int main()
         window.draw(circle);
         window.display();
     }
+}
+
+int main()
+{
+    window.setFramerateLimit(FPS);
+    sf::CircleShape circle(RAD);
+    circle.setOutlineThickness(5); // устанавливаем толщину контура круга
+    circle.setOutlineColor(sf::Color(80, 220, 50)); // устанавливаем цвет контура
+    
+
+    zalupa(circle);
+    
+
+        
 }
